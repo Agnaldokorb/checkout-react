e8d8d8adc9d00858e99fc989b8d9cc8208da78e5
Agnaldo Korb <agnaldokorb@gmail.com>
2026-09-07 12:08:08 -0300

    feat: criada lista de produtos

---
+++ src/data/produtos.js
@@ -0,0 +1,40 @@
+import { Tv, BoomBox, Mic, Disc3 } from "lucide-react"
+
+export const produtos = [
+    {
+        id: 1,
+        nome: "TV 32 AOC",
+        categoria: "Eletrodomestico",
+        preco: 1320.99,
+        quantidade: 2,
+        cor: "Preta",
+        Icone: Tv,
+    },
+    {
+        id: 2,
+        nome: "Som BoomBox 2 JBL",
+        categoria: "Som Portatil",
+        preco: 1859.99,
+        quantidade: 1,
+        cor: "Verde",
+        Icone: BoomBox,
+    },
+    {
+        id: 3,
+        nome: "Microfone PC",
+        categoria: "Eletronicos",
+        preco: 56.99,
+        quantidade: 5,
+        cor: "Branco",
+        Icone: Mic,
+    },
+    {
+        id: 4,
+        nome: "CD Hungria Hip Hop",
+        categoria: "Discos",
+        preco: 156.85,
+        quantidade: 10,
+        cor: "NA",
+        Icone: Disc3,
+    },
+]
